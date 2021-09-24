5a3510640c100f9d0564c6c5e0135c115b629d37
Douglas Soule <souleman13@gmail.com>
2021-09-23 19:32:36 -0600

    add password to mongodb call

--- lambda/createNewUserHook/createNewUserHook.ts
+++ lambda/createNewUserHook/createNewUserHook.ts
@@ -6,9 +6,9 @@ const { MongoClient } = require('mongodb');
 export const handler: Handler = async (event: any, context: any) => {
   const { name = '', email = ''  } = event.queryStringParameters
 
-  const uri = "mongodb+srv://admin:<password>@cluster0.acggh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  const uri = "mongodb+srv://admin:r6L9wGfIsuILdZVI@cluster0.acggh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
   const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-  await client.connect(() => {
+  await client.connect((err: any) => {
     const collection = client.db("gamer-id-db").collection("users");
     const newUser = {
       name,
@@ -16,6 +16,7 @@ export const handler: Handler = async (event: any, context: any) => {
     }
     collection.insert(newUser)
     client.close();
+    if(err) console.log(err)
   });
 
   return {
