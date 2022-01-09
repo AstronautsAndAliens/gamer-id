59c9291822b4568c8a56b79c7fcde58b3459add1
Douglas Soule <souleman13@gmail.com>
2022-01-08 22:55:25 -0700

    add steam lambda funcs

--- lambda/createNewUserHook/createNewUserHook.ts
+++ lambda/createNewUserHook/createNewUserHook.ts
@@ -1,41 +1,57 @@
 import { Handler } from '@netlify/functions'
 const { MongoClient } = require('mongodb')
 import { v4 as uuidv4 } from 'uuid'
-import { uniqueNamesGenerator, NumberDictionary, adjectives, colors, animals, starWars } from 'unique-names-generator';
-import { IPersona } from 'src/app/models/persona.model';
-
+import {
+  uniqueNamesGenerator,
+  NumberDictionary,
+  adjectives,
+  colors,
+  animals,
+  starWars,
+} from 'unique-names-generator'
+import { IPersona } from 'src/app/models/persona.model'
 
 //connected to Auth0, Auth Pipeline, Hook, Post-User Registration
 //endpoint should be called everytime a new user is registered with auth0 db
 //saves information from auth0 user to db to begin account setup
 export const handler: Handler = async (event: any, context: any) => {
   //these values come from a post-registration action on auth0 side, controlled by dashboard
-  const { user_id = '', email = '', gamer_id = ''  } = event.queryStringParameters
+  const {
+    user_id = '',
+    email = '',
+    gamer_id = '',
+  } = event.queryStringParameters
 
   const newPersona: IPersona = {
     id: uuidv4(),
     auth0_user_id: user_id,
     gamer_id,
     email,
-    nickname: generateNickname()
+    nickname: generateNickname(),
   }
-  const client = new MongoClient(process.env['MONGODB_CONNECTION_STRING'], { useNewUrlParser: true, useUnifiedTopology: true })
-
+  const client = new MongoClient(process.env['MONGODB_CONNECTION_STRING'], {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   await client.connect()
-  await client.db(process.env['MONGODB_DB_NAME']).collection(process.env['MONGODB_COLLECTION_PERSONA']).insertOne(newPersona)
-
+  await client
+    .db(process.env['MONGODB_DB_NAME'])
+    .collection(process.env['MONGODB_COLLECTION_PERSONA'])
+    .insertOne(newPersona)
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'success! new persona created in db', newPersona }),
+    body: JSON.stringify({
+      message: 'success! new persona created in db',
+      newPersona,
+    }),
   }
 }
-
 const generateNickname = (): string => {
-  const numbers = NumberDictionary.generate({ min: 100, max: 999 });
+  const numbers = NumberDictionary.generate({ min: 100, max: 999 })
   return uniqueNamesGenerator({
     dictionaries: [adjectives, colors, animals, starWars, numbers],
     separator: '',
     length: 3,
-    style: 'capital'
+    style: 'capital',
   })
 }
--- lambda/update-persona-steam-id/update-persona-steam-id.ts
+++ lambda/update-persona-steam-id/update-persona-steam-id.ts
@@ -2,8 +2,8 @@ import { Handler } from '@netlify/functions'
 import { IPersona } from 'src/app/models/persona.model'
 const { MongoClient } = require('mongodb')
 
-export const handler: Handler = async (event: any, _context: any) => {
-  const { gamer_id = '', nickname = '' } = event.queryStringParameters
+export const handler: Handler = async (event: any, _context) => {
+  const { gamer_id = '', steam_id = '' } = event.queryStringParameters
   const client = new MongoClient(process.env['MONGODB_CONNECTION_STRING'], {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,7 +12,7 @@ export const handler: Handler = async (event: any, _context: any) => {
   const persona: IPersona = await client
     .db(process.env['MONGODB_DB_NAME'])
     .collection(process.env['MONGODB_COLLECTION_PERSONA'])
-    .updateOne({ gamer_id: gamer_id }, { $set: { nickname: nickname } })
+    .updateOne({ gamer_id: gamer_id }, { $set: { steam_id: steam_id } })
   console.log(persona)
   return {
     statusCode: 200,
